ec42313ed4e580f71ad5f03cfc19796d90e55a07
Chris Birch <datapointchris@gmail.com>
2022-01-29 11:39:30 -0700

    Change small styling

--- apartment_comparison/helpers.py
+++ apartment_comparison/helpers.py
@@ -39,7 +39,7 @@ FEATURE_TYPE_MAP = {
 def convert_data_types_from_strings(info, mapping):
     converted = {}
     for k, v in info.items():
-        if v == '' or v is None:
+        if v == '' or v is None or v == 'None':
             converted[k] = None
         else:
             data_type = mapping.get(k)
